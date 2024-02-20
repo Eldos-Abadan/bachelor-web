9bf48b63e59b37fcaf7102850b9b0833cc2ce803
Eldos <is21d003@mandakh.edu.mn>
2024-02-20 17:54:16 +0800

    Eldos 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { text } from "stream/consumers";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div className="w-screen h-[80vh] bg-white flex flex-row">
-      <div className="w-3/5 h-full bg-green-500"></div>
-      <div className="w-2/5 h-full bg-black"></div>
+      <div className="w-3/5 h-full bg-primary"></div>
+      <div className="w-2/5 h-full bg-black">X</div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        "primary": "#2b891c"
+        "primary": "#A6FF8B"
       }
     },
   },
