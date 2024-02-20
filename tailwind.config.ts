909b420ece51c738d03224d9d97d1df5ca0bfcf6
Eldos <is21d003@mandakh.edu.mn>
2024-02-20 18:22:56 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        "primary": "#A6FF8B"
+        "primary": "#A6FF8B "
       }
     },
   },
